ce42c8d389a896249b6fa8b8533a59b29ff76b59
Gev2209 <mirzoyan.gevorg.23@gmail.com>
2025-08-28 15:00:17 +0400

    add repeat_password check

--- middleware/checkPassword.js
+++ middleware/checkPassword.js
@@ -1,20 +1,22 @@
-const fs = require('fs').promises;
-const path = require('path');
+    const fs = require('fs').promises;
+    const path = require('path');
 
-const checkPass = async (req, res, next) => {
-    try {
-        const users = await fs.readFile(path.join(__dirname, '..', 'db', 'users.json'), 'utf-8');
-        res.locals.users = JSON.parse(users);
-        const {password} = req.body;
-        if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{5,}$/.test(password)) {
-            // res.status(404).json({message: 'Password must be at least 5 characters long, contain at least one uppercase letter, one lowercase letter, and one number'});
-            return res.render('register', { title: 'Register', error: 'Password must be at least 5 characters long, contain at least one uppercase letter, one lowercase letter, and one number' })
-        } else {
+    const checkPass = async (req, res, next) => {
+        try {
+            const users = await fs.readFile(path.join(__dirname, '..', 'db', 'users.json'), 'utf-8');
+            res.locals.users = JSON.parse(users);
+            const {password, repeat_password} = req.body;
+            if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{5,}$/.test(password)) {
+                // res.status(404).json({message: 'Password must be at least 5 characters long, contain at least one uppercase letter, one lowercase letter, and one number'});
+                return res.render('register', { title: 'Register', error: 'Password must be at least 5 characters long, contain at least one uppercase letter, one lowercase letter, and one number' })
+            }
+            if (password !== repeat_password) {
+                return res.render('register', { title: 'Register', error: 'Passwords do not match'})
+            }
             return next();
+        } catch (error) {
+            res.status(404).json({error: 'Internal server error'});
         }
-    } catch (error) {
-        res.status(404).json({error: 'Internal server error'});
     }
-}
 
-module.exports.checkPass = checkPass
+    module.exports.checkPass = checkPass
